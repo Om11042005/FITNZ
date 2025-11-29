a816e625136ee7300e67021e520960f00ec6525a
Ahmdavadi <omshripalpatel@gmail.com>
2025-11-29 17:39:29 +1300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-# All database connection details for FIT NZ project.
-
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Apple@2310",  # change this if needed
-    "database": "fit_nz"    # main database name for FIT NZ
-}
